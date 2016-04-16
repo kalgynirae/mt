9c648c3511850bbeb57717abe58bcc45739e2e80
Colin Chan <colinchan@lumeh.org>
2016-04-16 13:23:50 -0700

    Add some debugging

--- mt.py
+++ mt.py
@@ -5,6 +5,8 @@ app.config.from_envvar('MT_CONFIG')
 
 @app.route('/webhook')
 def webhook():
+    print(app.config['token'])
+    print(request.form)
     if request.form['hub.verify_token'] == app.config['token']:
         return request.form['hub.challenge']
     raise ValueError('Wrong token')
