4b1c8d483b144a149cd3a46f2a786e18e38dd77c
Colin Chan <colinchan@lumeh.org>
2016-04-16 15:17:20 -0700

    Fix bugs

--- mt.py
+++ mt.py
@@ -1,12 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 
 app = Flask(__name__)
 app.config.from_envvar('MT_CONFIG')
 
 @app.route('/webhook')
 def webhook():
-    print(app.config['token'])
-    print(request.form)
-    if request.form['hub.verify_token'] == app.config['token']:
-        return request.form['hub.challenge']
-    raise ValueError('Wrong token')
+    if request.args['hub.verify_token'] == app.config['TOKEN']:
+        return Response(request.args['hub.challenge'], mimetype='text/plain')
+    return Response('wrong token', mimetype='text/plain')
