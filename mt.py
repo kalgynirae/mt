614f01d21d8c344e61da0a51518d568ef8a9284e
Colin Chan <colinchan@lumeh.org>
2016-04-16 17:15:12 -0700

    Add stuff

--- mt.py
+++ mt.py
@@ -1,10 +1,47 @@
+import logging
+
 from flask import Flask, request, Response
+import requests
 
 app = Flask(__name__)
 app.config.from_envvar('MT_CONFIG')
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: $(message)s'))
+handler.setLevel(logging.DEBUG)
+app.logger.addHandler(handler)
 
-@app.route('/webhook')
-def webhook():
-    if request.args['hub.verify_token'] == app.config['TOKEN']:
-        return Response(request.args['hub.challenge'], mimetype='text/plain')
+@app.route('/test')
+def test():
+    raise RuntimeError('test')
+
+@app.route('/webhook', methods=['GET'])
+def get():
+    if request.args.get('hub.verify_token') == app.config['TOKEN']:
+        return Response(request.args.get('hub.challenge'), mimetype='text/plain')
     return Response('wrong token', mimetype='text/plain')
+
+@app.route('/webhook', methods=['POST'])
+def post():
+    for sender, text in extract_text_messages(request.get_json()):
+        response = 'You said “%s”' % text
+        data = {
+            'recipient': {'id': sender},
+            'message': {'text': response},
+        }
+        params = {
+            'access_token': app.config['PAGE_ACCESS_TOKEN'],
+        }
+        requests.post(
+            'https://graph.facebook.com/v2.6/me/messages',
+            params=params,
+            data=data,
+        )
+
+def extract_text_messages(data):
+    for entry in data['entry']:
+        for message in entry['messaging']:
+            if message['message'] and message['message']['text']:
+                yield message['sender']['id'], message['message']['text']
+
+if __name__ == '__main__':
+    app.run(debug=True)
