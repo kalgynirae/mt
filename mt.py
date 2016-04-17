be7f6a6fb8efb0f01da49012e666b0fd8c5c0b2c
Colin Chan <colinchan@lumeh.org>
2016-04-16 17:24:51 -0700

    Add some logging

--- mt.py
+++ mt.py
@@ -18,7 +18,7 @@ def test():
 def get():
     if request.args.get('hub.verify_token') == app.config['TOKEN']:
         return Response(request.args.get('hub.challenge'), mimetype='text/plain')
-    return Response('wrong token', mimetype='text/plain')
+    return 'wrong token'
 
 @app.route('/webhook', methods=['POST'])
 def post():
@@ -31,11 +31,14 @@ def post():
         params = {
             'access_token': app.config['PAGE_ACCESS_TOKEN'],
         }
-        requests.post(
+        r = requests.post(
             'https://graph.facebook.com/v2.6/me/messages',
             params=params,
             data=data,
         )
+        app.logger.debug('graph.facebook.com returned %s', r.status_code)
+        app.logger.debug('Response from graph.facebook.com: %r', r.text)
+    return 'ok'
 
 def extract_text_messages(data):
     for entry in data['entry']:
